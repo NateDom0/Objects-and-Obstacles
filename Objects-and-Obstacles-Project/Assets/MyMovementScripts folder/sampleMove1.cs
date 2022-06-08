1fdfdeed0f4b660d8f7fc6839e8ccef18d3ffca1
Nathaniel Doromal <nathanielrd98@gmail.com>
2022-06-08 01:43:42 -0700

    Added unity project to repo

---
+++ Objects-and-Obstacles-Project/Assets/MyMovementScripts folder/sampleMove1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sampleMove1 : MonoBehaviour   //class name similar to file name in assets folder
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Translate(new Vector3(1,1,0) * Time.deltaTime * speed);
+        }
+    }
+}
